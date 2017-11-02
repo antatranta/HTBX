9f00ec1671baa7e312c6cf79b52bf2061c4cb586
liwen <w_li6@u.pacific.edu>
2017-11-02 16:49:30 -0700

    put keyboard events to game console.

--- src/GameConsole.java
+++ src/GameConsole.java
@@ -1,6 +1,58 @@
+import java.awt.event.KeyEvent;
 import java.util.*;
 
-public class GameConsole {
+import acm.program.GraphicsProgram;
+
+public class GameConsole extends GraphicsProgram{
 	private ArrayList<Ship> ships = new ArrayList<Ship>();
+	private PlayerShip player;
 	
+	
+	
+	
+	
+	//
+    public void keyPressed(KeyEvent e) {
+
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_LEFT) {
+            player.setDx(-1);
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+        	player.setDx(1);
+        }
+
+        if (key == KeyEvent.VK_UP) {
+        	player.setDy(-1);
+        }
+
+        if (key == KeyEvent.VK_DOWN) {
+        	player.setDy(1);
+        }
+    }
+
+    public void keyReleased(KeyEvent e) {
+        
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_LEFT) {
+        	player.setDx(0);
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+        	player.setDx(0);
+        }
+
+        if (key == KeyEvent.VK_UP) {
+        	player.setDy(0);
+        }
+
+        if (key == KeyEvent.VK_DOWN) {
+        	player.setDy(0);
+        }
+    }
 }
+
+
--- src/PlayerShip.java
+++ src/PlayerShip.java
@@ -58,47 +58,29 @@ public class PlayerShip extends Ship{
 	        y += dy;
 		}
 	}
+	public int getDx() {
+		return dx;
+	}
+	public void setDx(int dx) {
+		this.dx = dx;
+	}
+	public int getDy() {
+		return dy;
+	}
+	public void setDy(int dy) {
+		this.dy = dy;
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
 	
-	
-    public void keyPressed(KeyEvent e) {
-
-        int key = e.getKeyCode();
-
-        if (key == KeyEvent.VK_LEFT) {
-            dx = -1;
-        }
-
-        if (key == KeyEvent.VK_RIGHT) {
-            dx = 1;
-        }
-
-        if (key == KeyEvent.VK_UP) {
-            dy = -1;
-        }
-
-        if (key == KeyEvent.VK_DOWN) {
-            dy = 1;
-        }
-    }
-
-    public void keyReleased(KeyEvent e) {
-        
-        int key = e.getKeyCode();
-
-        if (key == KeyEvent.VK_LEFT) {
-            dx = 0;
-        }
-
-        if (key == KeyEvent.VK_RIGHT) {
-            dx = 0;
-        }
-
-        if (key == KeyEvent.VK_UP) {
-            dy = 0;
-        }
-
-        if (key == KeyEvent.VK_DOWN) {
-            dy = 0;
-        }
-    }
 }
