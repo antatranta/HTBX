6b509774f74af0bd4c026fd4fdd57d7dd77a6a83
Anthony Tran <a_tran38@u.pacific.edu>
2017-11-02 01:15:49 -0700

    Added Mouse Hover response also probably going towards mouse only menu

--- src/MenuItem.java
+++ src/MenuItem.java
@@ -1,25 +1,41 @@
+import javafx.beans.binding.Bindings;
 import javafx.geometry.Pos;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.scene.effect.BoxBlur;
+import javafx.scene.effect.Glow;
 
+import javafx.scene.effect.Effect;
+
+// TODO: REWRITE TO FORMAT INTO MOUSE ONLY INPUT
+// SINCE FOUND OUT IT IS PRETTY HARD TO CODE BOTH KEYBOARD AND MOSUE INPUT
 public class MenuItem extends HBox {
 	private Text text;
 	private Runnable script;
 	
 	private static final Font font = Font.font("", FontWeight.BOLD, 24);
+	private Effect glow = new Glow(1);
+    private Effect noGlow = new Glow(0);
 	
 	public MenuItem(String menuName) {
 		super(15);
-		//hoverProperty().addListener(null);
-		// TODO add mouseHover and mouseClick
 		setAlignment(Pos.CENTER);
-		
 		text = new Text(menuName);
 		text.setFont(font);
 		
+		/*text.fillProperty().bind(
+				Bindings.when(hoverProperty())
+						.then(Color.WHITE)
+						.otherwise(Color.GREY)
+		);*/
+		text.effectProperty().bind(
+                Bindings.when(hoverProperty())
+                        .then(glow)
+                        .otherwise(noGlow)
+        );
 		getChildren().add(text);
 		setActive(false);
 		setOnActivate(() -> System.out.println(menuName + " activated"));
