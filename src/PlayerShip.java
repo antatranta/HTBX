caeb9f98d4646fad144a80d3ff2aa34a691a2486
liwen <w_li6@u.pacific.edu>
2017-11-02 16:57:09 -0700

    put x y coordinate to ship class.

--- src/PlayerShip.java
+++ src/PlayerShip.java
@@ -1,38 +1,11 @@
-import java.awt.Image;
-import java.awt.event.KeyEvent;
-import javax.swing.ImageIcon;
-
-import acm.graphics.GImage;
-import acm.program.GraphicsProgram;
-
 public class PlayerShip extends Ship{
 	private int current_shield;
 	
-	private int dx;
-	private int dy;
-	private int x;
-	private int y;
-	private GImage playership;
-	
-	public PlayerShip(PhysXObject physObj, int current_health, ShipStats stats,int dx, int dy, int x, int y) {
-		super(physObj, current_health, stats);
+	public PlayerShip(PhysXObject physObj, int current_health, ShipStats stats, int x, int y) {
+		super(physObj, current_health, stats, x, y);
 		this.current_shield = current_shield;
-		this.dy = dy;
-		this.dx = dx;
-		this.x = x;
-		this.y = y;
-		initShip();
 	}
-	private void initShip() {
-		//ImageIcon ii = new ImageIcon("craft.png");
-		//image = ii.getImage();
-		x = 100;
-		y = 100;
-		//GImage playership = new GImage("triangle.png",x,y);
-		//add(playership);
 
-	}
-	
 	public void ChargeShield() {
 		
 	}
@@ -50,37 +23,6 @@ public class PlayerShip extends Ship{
 
 	public void setCurrent_shield(int current_shield) {
 		this.current_shield = current_shield;
-	}
-	
-	public void move() {
-		while(getCurrent_health()>0) {
-	        x += dx;
-	        y += dy;
-		}
-	}
-	public int getDx() {
-		return dx;
-	}
-	public void setDx(int dx) {
-		this.dx = dx;
-	}
-	public int getDy() {
-		return dy;
-	}
-	public void setDy(int dy) {
-		this.dy = dy;
-	}
-	public int getX() {
-		return x;
-	}
-	public void setX(int x) {
-		this.x = x;
-	}
-	public int getY() {
-		return y;
-	}
-	public void setY(int y) {
-		this.y = y;
 	}
 	
 }
--- src/Ship.java
+++ src/Ship.java
@@ -5,10 +5,17 @@ public class Ship {
 	private int current_health;
 	private ShipStats stats;
 	
-	public Ship(PhysXObject physObj,int current_health, ShipStats stats) {
+	private int dx = 0;// 1 to right, -1 to left.
+	private int dy = 0;// 1 to up, -1 to down.
+	private int x;// ship x coordinate
+	private int y;// ship y coordinate
+	
+	public Ship(PhysXObject physObj,int current_health, ShipStats stats,int x,int y) {
 		this.physObj = new PhysXObject();
 		this.setCurrent_health(current_health);
 		this.stats = new ShipStats(1, 1, 1, 1);//speed, shield_max, health_max, damage
+		this.x = x;
+		this.y = y;
 	}
 	
 	public PhysXObject getPhysObj() {
@@ -29,11 +36,7 @@ public class Ship {
 		}else {
 			setCurrent_health(0);
 		}
-	}
-	
-	public void Move() {
-
-	}
+	}	
 
 	public int getCurrent_health() {
 		return current_health;
@@ -41,5 +44,37 @@ public class Ship {
 
 	public void setCurrent_health(int current_health) {
 		this.current_health = current_health;
+	}
+	
+	public void Move() {
+		while(getCurrent_health()>0) {
+	        x += dx;
+	        y += dy;
+		}
+	}
+	
+	public int getDx() {
+		return dx;
+	}
+	public void setDx(int dx) {
+		this.dx = dx;
+	}
+	public int getDy() {
+		return dy;
+	}
+	public void setDy(int dy) {
+		this.dy = dy;
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
 	}
 }
