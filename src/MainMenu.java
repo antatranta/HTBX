746dfc30f12844771d9a47ad30ca04236f1db81e
Anthony Tran <a_tran38@u.pacific.edu>
2017-11-02 17:16:07 -0700

    Deleted some code so it is now only mouse input

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -2,7 +2,6 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Rectangle;
@@ -15,7 +14,6 @@ public class MainMenu extends Application {
 	public static final int WINDOW_HEIGHT = 600;
 	
 	private VBox menuOptions;
-	private int currentOption = 0;
 	
 	private Parent createContent() {
 		StackPane root = new StackPane();
@@ -30,43 +28,16 @@ public class MainMenu extends Application {
 		menuOptions = new VBox(10, new MenuItem("PLAY"), new MenuItem("SETTINGS"), new MenuItem("SCORES"), new MenuItem("CONTROLS"), quit);
 		menuOptions.setAlignment(Pos.CENTER);
 		
-		getMenuItem(0).setActive(true);
-		
 		root.getChildren().addAll(backGround, menuOptions);
 		StackPane.setAlignment(menuOptions, Pos.CENTER);
 		return root;
 	}
-	
-	private MenuItem getMenuItem(int index) {
-        return (MenuItem)menuOptions.getChildren().get(index);
-    }
 	
 	// Main application that brings up the main menu
 	// As of right now the only way to control the menu is through WASD and ENTER
 	@Override
 	public void start(Stage primaryStage) {
 		Scene scene = new Scene(createContent());
-		
-		scene.setOnKeyPressed(event -> {
-			if(event.getCode() == KeyCode.W) {
-                if(currentOption > 0) {
-                    getMenuItem(currentOption).setActive(false);
-                    getMenuItem(--currentOption).setActive(true);
-                }
-            }
-
-            if(event.getCode() == KeyCode.S) {
-                if(currentOption < menuOptions.getChildren().size() - 1) {
-                    getMenuItem(currentOption).setActive(false);
-                    getMenuItem(++currentOption).setActive(true);
-                }
-            }
-
-            if(event.getCode() == KeyCode.ENTER) {
-                getMenuItem(currentOption).activate();
-            }
-		});
-		
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("HTBX");
 		primaryStage.setResizable(false);
--- src/MenuItem.java
+++ src/MenuItem.java
@@ -5,20 +5,13 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
-import javafx.scene.effect.BoxBlur;
-import javafx.scene.effect.Glow;
-
-import javafx.scene.effect.Effect;
 
 // TODO: REWRITE TO FORMAT INTO MOUSE ONLY INPUT
 // SINCE FOUND OUT IT IS PRETTY HARD TO CODE BOTH KEYBOARD AND MOSUE INPUT
 public class MenuItem extends HBox {
 	private Text text;
-	private Runnable script;
 	
 	private static final Font font = Font.font("", FontWeight.BOLD, 24);
-	private Effect glow = new Glow(1);
-    private Effect noGlow = new Glow(0);
 	
 	public MenuItem(String menuName) {
 		super(15);
@@ -26,33 +19,17 @@ public class MenuItem extends HBox {
 		setMaxWidth(0);
 		text = new Text(menuName);
 		text.setFont(font);
-		
-		/*text.fillProperty().bind(
+		text.fillProperty().bind(
 				Bindings.when(hoverProperty())
 						.then(Color.WHITE)
 						.otherwise(Color.GREY)
-		);*/
-		text.effectProperty().bind(
-                Bindings.when(hoverProperty())
-                        .then(glow)
-                        .otherwise(noGlow)
-        );
+		);
+		
 		getChildren().add(text);
-		setActive(false);
 		setOnActivate(() -> System.out.println(menuName + " activated"));
 	}
 	
-	public void setActive(boolean bool) {
-		text.setFill(bool ? Color.WHITE : Color.GREY);
-	}
-	
 	public void setOnActivate(Runnable run) {
-		script = run;
-	}
-	
-	public void activate() {
-		if (script != null) {
-			script.run();
-		}
+		setOnMouseClicked(e -> run.run());
 	}
 }
