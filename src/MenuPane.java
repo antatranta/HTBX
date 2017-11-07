01b16bd8123e8d8cf41425c0e1f6782ec95d9690
Anthony Tran <a_tran38@u.pacific.edu>
2017-11-06 23:36:28 -0800

    Centered images and text so it looks neater

--- src/MenuPane.java
+++ src/MenuPane.java
@@ -9,36 +9,38 @@ import acm.graphics.GObject;
 import acm.graphics.GImage;
 
 public class MenuPane extends GraphicsPane {
-	private static final Font font = new Font("", Font.BOLD, 20);	
+	private static final Font font = new Font("", Font.BOLD, 20);
+	private static final double CENTER_WIDTH = MainApplication.WINDOW_WIDTH / 2;
+	private static final double CENTER_HEIGHT = MainApplication.WINDOW_HEIGHT / 2;
 	
-	private static double yPos = 225; // used for keeping a yPos for main menu and incrementing for other options in the main menu
 	private MainApplication program; //you will use program to get access to all of the GraphicsProgram calls
 	private ArrayList<GLabel> mainMenu = new ArrayList<GLabel>();
 	private GImage title;
+	private GObject obj;
 	// TODO possible constant of xPos
 	
 	public MenuPane(MainApplication app) {
 		program = app;
-		title = new GImage("HTBX_Title.png", MainApplication.WINDOW_WIDTH / 4.5, 0);
-		double yPosChange = yPos;
+		title = new GImage("HTBX_Title.png");
+		title.setLocation(CENTER_WIDTH - (title.getWidth() / 2), 50);
 		
 		GLabel play = new GLabel("PLAY");
 		GLabel settings = new GLabel("SETTINGS");
 		GLabel scores = new GLabel("SCORES");
 		GLabel controls = new GLabel("CONTROLS");
 		GLabel quit = new GLabel("QUIT");
-		
 		mainMenu.add(play);
 		mainMenu.add(settings);
 		mainMenu.add(scores);
 		mainMenu.add(controls);
 		mainMenu.add(quit);
 		
+		double yPos = CENTER_HEIGHT - (play.getHeight() / 2);
 		for(GLabel menu:mainMenu) {
 			menu.setFont(font);
-			menu.setLocation(345, yPosChange);
+			menu.setLocation((MainApplication.WINDOW_WIDTH / 2) - (menu.getWidth() / 2), yPos);
 			menu.setColor(Color.black);
-			yPosChange += 50;
+			yPos += 50;
 		}
 	}
 	
@@ -61,7 +63,7 @@ public class MenuPane extends GraphicsPane {
 	// Main menu options switches panels and/or quits - Anthony
 	@Override
 	public void mousePressed(MouseEvent e) {
-		GObject obj = program.getElementAt(e.getX(), e.getY());
+		obj = program.getElementAt(e.getX(), e.getY());
 		for(GLabel menu:mainMenu) {
 			if(obj == menu) {
 				switch(menu.getLabel()) {
@@ -94,7 +96,7 @@ public class MenuPane extends GraphicsPane {
 	// When you hover the mouse over the text it highlights :D - Anthony
 	@Override
 	public void mouseMoved(MouseEvent e) {
-		GObject obj = program.getElementAt(e.getX(), e.getY());
+		obj = program.getElementAt(e.getX(), e.getY());
 		for(GLabel menu:mainMenu) {
 			if(obj == menu) {
 				menu.setColor(Color.gray);
