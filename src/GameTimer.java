454b88c0e2f4d4f4ad3ec4572d31d5ab0211e97c
Anthony Tran <a_tran38@u.pacific.edu>
2017-11-07 22:05:43 -0800

    Spacing

--- src/GameTimer.java
+++ src/GameTimer.java
@@ -2,6 +2,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.Timer;
+
 public class GameTimer implements ActionListener{
 	private Timer gameTimer;
 	private boolean callEvents = false;
