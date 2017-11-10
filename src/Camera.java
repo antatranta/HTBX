6cf1a8e4eb56367e5bac293cbc295e782a2b1f1e
LouisIV <Louislombardoiv@gmail.com>
2017-11-10 12:06:45 -0800

    Setup transformations

--- src/Camera.java
+++ src/Camera.java
@@ -1,4 +1,28 @@
 
 public class Camera {
-
+	private static float FORWARD_RATIO;
+	private static float BACKWARD_RATIO;
+	
+	public void init() {
+		
+		// Set ratio to 1:1 to start
+		FORWARD_RATIO = BACKWARD_RATIO = 1;
+	}
+	
+	public static float getForwardRatio() {
+		return FORWARD_RATIO;
+	}
+	
+	public static float getBackwardRatio() {
+		return BACKWARD_RATIO;
+	}
+	
+	public static Vector2 frontendToBackend(Vector2 pos) {
+		return new Vector2(pos.getX() * BACKWARD_RATIO, pos.getY() * BACKWARD_RATIO);
+	}
+	
+	public static Vector2 backendToFrontend(Vector2 pos) {
+		return new Vector2(pos.getX() * FORWARD_RATIO, pos.getY() * FORWARD_RATIO);
+	}
+	
 }
