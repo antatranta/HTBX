5a643bfaa7b5e886e80129d956b803ea67027593
LouisIV <Louislombardoiv@gmail.com>
2017-11-11 18:06:51 -0800

    Drawing was not physX based

--- src/GamePane.java
+++ src/GamePane.java
@@ -39,6 +39,19 @@ public class GamePane extends GraphicsPane implements ActionListener, KeyListene
 	private PlayerShip player;
 	private Vector2 last_mouse_loc;
 	
+	private GLabel CURRENT_QUID_LABEL;
+	private GLabel CURRENT_PLAYER_POS_LABEL;
+	private GLabel CURRENT_MOUSE_POS_LABEL;
+	private GLabel CURRENT_ASTEROIDS_LABEL;
+	private GRect DEBUGGING_BOX;
+	
+	private ArrayList<GOval> POINT_TEST;
+	
+	private ArrayList<StaticRect> DEBUGGING_LINES;
+	
+	private ArrayList<StaticRect> DEBUGGING_ROWS;
+	private ArrayList<StaticRect> DEBUGGING_COLS;
+	
 	private boolean CAN_MOVE = false;
 	private boolean MOVEMENT_LOCK = false;
 	private float MOVEMENT_CONSTANT = .0000001f;
@@ -53,21 +66,110 @@ public class GamePane extends GraphicsPane implements ActionListener, KeyListene
 	private float xAxis = 0;
 	private float yAxis = 0;
 	private int track_amount = 0;
+	
+	private Vector2 TRACKING_POSITION;
 
 	//private Vector2 combat_offset = new Vector2(0,0); Unused for now; planned for centering player post combat smoothly
 	
 	public GamePane(MainApplication app) {
 		this.program = app;
 		init();
+		TRACKING_POSITION = player.getPhysObj().getPosition();
+		setOffset();
+	}
+	
+	public void setupDebug() {
+   		program.add(CURRENT_QUID_LABEL);
+		program.add(DEBUGGING_BOX);
+		program.add(CURRENT_ASTEROIDS_LABEL);
+		program.add(CURRENT_PLAYER_POS_LABEL);
+		program.add(CURRENT_MOUSE_POS_LABEL);
+		
+		DEBUGGING_BOX.setFillColor(Color.black);
+		DEBUGGING_BOX.setFilled(true);
+		
+		CURRENT_QUID_LABEL.setColor(Color.white);
+		CURRENT_QUID_LABEL.setFont("Arial");
+		
+		CURRENT_ASTEROIDS_LABEL.setColor(Color.white);
+		CURRENT_ASTEROIDS_LABEL.setFont("Arial");
+		
+		CURRENT_PLAYER_POS_LABEL.setColor(Color.WHITE);
+		CURRENT_PLAYER_POS_LABEL.setFont("Arial");
+		
+		CURRENT_MOUSE_POS_LABEL.setColor(Color.WHITE);
+		CURRENT_MOUSE_POS_LABEL.setFont("Arial");
+		
+		DEBUGGING_ROWS = new ArrayList<StaticRect>();
+		DEBUGGING_COLS = new ArrayList<StaticRect>();
+		
+		DEBUGGING_LINES = new ArrayList<StaticRect>();
+		
+		
+		for(int i =0; i < PhysXLibrary.MAP_WIDTH; ++i) {
+//			DEBUGGING_ROWS.add(new StaticRect(new Vector2((PhysXLibrary.QUADRANT_WIDTH * i), -PhysXLibrary.getMapHeight()), new Vector2(5, PhysXLibrary.getMapHeight())));
+			DEBUGGING_ROWS.add(new StaticRect(new Vector2(-(PhysXLibrary.QUADRANT_WIDTH * i), 0), new Vector2(5, PhysXLibrary.getMapHeight())));
+			program.add(DEBUGGING_ROWS.get(i).getRect());
+			DEBUGGING_ROWS.get(i).getRect().setFillColor(Color.LIGHT_GRAY);
+			DEBUGGING_ROWS.get(i).getRect().setFilled(true);
+		}
+		
+		for(int i =0; i < PhysXLibrary.MAP_HEIGHT; ++i) {
+			DEBUGGING_COLS.add(new StaticRect(new Vector2(0, -(PhysXLibrary.QUADRANT_HEIGHT * i)), new Vector2(PhysXLibrary.getMapWidth(), 5)));
+			program.add(DEBUGGING_COLS.get(i).getRect());
+			DEBUGGING_COLS.get(i).getRect().setFillColor(Color.LIGHT_GRAY);
+			DEBUGGING_COLS.get(i).getRect().setFilled(true);
+		}
+		
+		
+		
+		setSpriteLayer(CURRENT_QUID_LABEL, 1);
+		setSpriteLayer(CURRENT_ASTEROIDS_LABEL, 1);
+		
+		setSpriteLayer(CURRENT_PLAYER_POS_LABEL, 1);
+		setSpriteLayer(CURRENT_MOUSE_POS_LABEL, 1);
+		setSpriteLayer(DEBUGGING_BOX, 6);
+	}
+	
+	public void pointTest(Vector2 pos) {
+		POINT_TEST = new ArrayList<GOval>();
+		
+		Vector2 player_pos = Camera.frontendToBackend(pos);
+		POINT_TEST.add(new GOval(player_pos.getX(), player_pos.getY(), 25, 25));
+		program.add(POINT_TEST.get(POINT_TEST.size() - 1));
+		POINT_TEST.get(POINT_TEST.size() - 1).setFillColor(Color.blue);
+		POINT_TEST.get(POINT_TEST.size() - 1).setFilled(true);
+		
+		Vector2 player_pos_to_front = Camera.backendToFrontend(player_pos);
+		
+		POINT_TEST.add(new GOval(player_pos_to_front.getX(), player_pos_to_front.getY(), 10, 10));
+		program.add(POINT_TEST.get(POINT_TEST.size() - 1));
+		POINT_TEST.get(POINT_TEST.size() - 1).setFillColor(Color.red);
+		POINT_TEST.get(POINT_TEST.size() - 1).setFilled(true);
+		
+		Vector2 player_pos_back = Camera.frontendToBackend(player_pos_to_front);
+		
+		POINT_TEST.add(new GOval(player_pos_back.getX(), player_pos_back.getY(), 5, 5));
+		program.add(POINT_TEST.get(POINT_TEST.size() - 1));
+		POINT_TEST.get(POINT_TEST.size() - 1).setFillColor(Color.green);
+		POINT_TEST.get(POINT_TEST.size() - 1).setFilled(true);
 	}
 	
 	public void init() {
 		CAN_MOVE = false;
+		
 		last_mouse_loc = new Vector2(0,0);
 		cursor_dots = new ArrayList <GOval>();
 		pressed_keys = new ArrayList <Integer>();
 		drawn_rocks = new ArrayList <Asteroid>();
 		drawn_ships = new ArrayList <GameImage>();
+		
+		CURRENT_QUID_LABEL = new GLabel("Current QUID", 15, 25);
+		CURRENT_ASTEROIDS_LABEL = new GLabel("Current ASTEROIDS", 15, 50);
+		CURRENT_PLAYER_POS_LABEL = new GLabel("Current P Position", 15, 75);
+		CURRENT_MOUSE_POS_LABEL = new GLabel("Current M Position", 15, 100);
+		DEBUGGING_BOX = new GRect(10, 10, 300, 100);
+		
 		
 		console = program.getGameConsole();
 		player = console.getPlayer();
@@ -84,11 +186,19 @@ public class GamePane extends GraphicsPane implements ActionListener, KeyListene
 		player.getPhysObj().setQUID(console.physx().assignQuadrant(player.getPhysObj().getPosition()));
 		console.physx().setActiveQuadrant(console.physx().assignQuadrant(player.getPhysObj().getPosition()));
 		
+		
+	
 		CAN_MOVE = true;
 	}
 	
 	public void centerPlayer() {
-		player_img.setLocation((MainApplication.WINDOW_WIDTH / 2) - (player_img.getWidth() / 2), (MainApplication.WINDOW_HEIGHT / 2) - (player_img.getHeight() /2));
+		Vector2 frontPos = Camera.backendToFrontend(player.getPhysObj().getPosition());
+		player_img.setLocationRespectSize(frontPos.getX(), frontPos.getY());
+//		player_img.setLocation((MainApplication.WINDOW_WIDTH / 2) - (player_img.getWidth() / 2), (MainApplication.WINDOW_HEIGHT / 2) - (player_img.getHeight() /2));
+	}
+	
+	public void setOffset() {
+		Camera.setOffset(Vector2.Zero().minus(TRACKING_POSITION));
 	}
 	
 	@Override
@@ -107,7 +217,7 @@ public class GamePane extends GraphicsPane implements ActionListener, KeyListene
 
 		//auto_fire.setInitialDelay(0);
 		//auto_fire.start();
-
+		pointTest(new Vector2(e.getX(), e.getY()));
 		GObject obj = program.getElementAt(e.getX(), e.getY());
 		if(obj == player_img) {
 			program.switchToMenu();
@@ -126,6 +236,8 @@ public class GamePane extends GraphicsPane implements ActionListener, KeyListene
 	// Every tick of the global game clock calls all visual drawing necessary
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		TRACKING_POSITION = player.getPhysObj().getPosition();
+		setOffset();
 		// Player shoot every tick
 //		System.out.println("Fired");
 		if (CAN_ALIGN && !ALIGNMENT_LOCK) {
@@ -139,6 +251,16 @@ public class GamePane extends GraphicsPane implements ActionListener, KeyListene
 		
 		// TESTING!!! NOT FINAL
 		drawAsteroids(console.getActiveAsteroids());
+		
+		if(console.IS_DEBUGGING) {
+			CURRENT_QUID_LABEL.setLabel("Current QUID: " + player.getPhysObj().getQUID().toString());
+			CURRENT_PLAYER_POS_LABEL.setLabel("Current Player V2: " + player.getPhysObj().getPosition().toString());
+			CURRENT_MOUSE_POS_LABEL.setLabel("Current Mouse V2: " + Camera.frontendToBackend(last_mouse_loc).toString());
+//			pointTest();
+			
+			drawStaticRect(DEBUGGING_ROWS);
+			drawStaticRect(DEBUGGING_COLS);
+		}
 		/*
 		ArrayList <Quadrant> quads = console.physx().getQuadrants();
 		for (int i = 0; i < quads.size(); i++) {
@@ -196,9 +318,11 @@ public class GamePane extends GraphicsPane implements ActionListener, KeyListene
 		
 		player_img.rotate(TURN_POWER * final_turn);
 		player.adjustAngle(TURN_POWER * -final_turn);
-		player.getPhysObj().getPosition().add(new Vector2(cos, sin));
+//		player.getPhysObj().getPosition().add(new Vector2(cos, sin));
 		player.moveVector2(new Vector2(cos, sin));
 		player.getPhysObj().setQUID(console.physx().assignQuadrant(player.getPhysObj().getPosition()));
+		Vector2 newFEPOS = Camera.backendToFrontend(player.getPhysObj().getPosition());
+		player_img.setLocationRespectSize(newFEPOS.getX(), newFEPOS.getY());
 		
 		if (xAxis > 0 + MOVEMENT_CONSTANT) {
 			player.adjustAngle(-TURN_POWER);
@@ -249,15 +373,48 @@ public class GamePane extends GraphicsPane implements ActionListener, KeyListene
 
 	}
 	
+	private Vector2 fromGPoint(GPoint point) {
+		return Camera.frontendToBackend(new Vector2((float)point.getX(), (float)point.getY()));
+	}
+	
+	private void drawStaticRect(ArrayList<StaticRect> lines) {
+		for (int i = 0; i < lines.size(); i++) {
+			// Get the offset
+			StaticRect rect = lines.get(i);
+//			float offset_x = rect.getPhysObj().getPosition().getX() - player.getPhysObj().getPosition().getX();
+//			float offset_y = rect.getPhysObj().getPosition().getY() - player.getPhysObj().getPosition().getY();
+			
+			// Make a proper vector2 location according to the camera zoom scale
+			Vector2 final_off = Camera.backendToFrontend(rect.getPhysObj().getPosition());
+
+			// Are we already drawing that rock?
+			if (!DEBUGGING_LINES.contains(rect)) {
+				DEBUGGING_LINES.add(rect);
+				setSpriteLayer(rect.getRect(), ROCK_LAYER);
+			}
+			
+			// Set its location according to the offset
+			if (DEBUGGING_LINES.contains(rect)) {
+				rect.getRect().setLocation(final_off.getX(), final_off.getY());
+			}
+			
+		}
+	}
+	
 	private void drawAsteroids(ArrayList<Asteroid> asteroids) {
+		
+		if(console.IS_DEBUGGING) {
+			CURRENT_ASTEROIDS_LABEL.setLabel("Current ASTER: " + asteroids.size());
+		}
 		for (int i = 0; i < asteroids.size(); i++) {
 			// Get the offset
 			Asteroid asteroid = asteroids.get(i);
-			float offset_x = asteroid.getPhysObj().getPosition().getX() - player.getPhysObj().getPosition().getX();
-			float offset_y = asteroid.getPhysObj().getPosition().getY() - player.getPhysObj().getPosition().getY();
+//			Vector2 offset = asteroid.getPhysObj().getPosition().minus(player.getPhysObj().getPosition());
+//			float offset_x = asteroid.getPhysObj().getPosition().getX() - player.getPhysObj().getPosition().getX();
+//			float offset_y = asteroid.getPhysObj().getPosition().getY() - player.getPhysObj().getPosition().getY();
 			
 			// Make a proper vector2 location according to the camera zoom scale
-			Vector2 final_off = Camera.backendToFrontend(new Vector2(offset_x, offset_y));
+			Vector2 frontEndPos = Camera.backendToFrontend(asteroid.getPhysObj().getPosition());
 
 			// Are we already drawing that rock?
 			if (!drawn_rocks.contains(asteroid)) {
@@ -268,7 +425,7 @@ public class GamePane extends GraphicsPane implements ActionListener, KeyListene
 			
 			// Set its location according to the offset
 			if (drawn_rocks.contains(asteroid)) {
-				asteroid.getSprite().setLocation(final_off.getX(), final_off.getY());
+				asteroid.getSprite().setLocation(frontEndPos.getX(), frontEndPos.getY());
 			}
 			
 		}
@@ -279,6 +436,7 @@ public class GamePane extends GraphicsPane implements ActionListener, KeyListene
 				program.remove(asteroid.getSprite());
 			}
 		}
+		
 	}
 	
 	// Might be a very taxing method. We can change to having a simple cursor at the mouse pointer. Luckily, won't draw more than 5 dots
@@ -346,12 +504,16 @@ public class GamePane extends GraphicsPane implements ActionListener, KeyListene
         	}
         }
         
-        if(key == KeyEvent.VK_L) {
+        if(key == KeyEvent.VK_L && !console.IS_DEBUGGING) {
         		console.startDebugView();
+        		setupDebug();
         }
         
-        if(key == KeyEvent.VK_K) {
+        if(key == KeyEvent.VK_K && console.IS_DEBUGGING) {
     			console.endDebugView();
+    			
+        		program.remove(CURRENT_QUID_LABEL);
+        		program.remove(DEBUGGING_BOX);
         }
         
         if(console.IS_DEBUGGING) {
@@ -366,6 +528,11 @@ public class GamePane extends GraphicsPane implements ActionListener, KeyListene
         		}
         		if(key == KeyEvent.VK_U && Camera.getBackwardRatio() != 1 && Camera.getForwardRatio() != 1) {
         			console.changeGraphicsRatio(1, 1);
+        		}
+        		if(key == KeyEvent.VK_M) {
+        			System.out.println("SNAPSHOT --- ");
+        			System.out.println("Asteroids to draw: " + console.getActiveAsteroids().size());
+        			System.out.println("Player QUID: (" + player.getPhysObj().getQUID().getX() +", " + player.getPhysObj().getQUID().getY() + ", " + player.getPhysObj().getQUID().Order() + ")");
         		}
         }
 
