12a97a75dc888bc019e21554762a9bfd33f3c7d4
LouisIV <Louislombardoiv@gmail.com>
2017-11-12 13:37:41 -0800

    Major updates

--- src/StaticGObject.java
+++ src/StaticGObject.java
@@ -1,19 +1,24 @@
 import acm.graphics.*;
 
 public class StaticGObject {
-	private Vector2 size;
+	private Vector2 imageSize;
 	private PhysXObject physObj;
 	private GOval[] objects;
 	
 	public StaticGObject(PhysXObject physObj) {
 		this.physObj = physObj;
-		this.size = Vector2.One();
+		this.imageSize = Vector2.One();
+	}
+	
+	public StaticGObject(PhysXObject physObj, Vector2 size) {
+		this.physObj = physObj;
+		this.imageSize = size;
 	}
 	
 	public StaticGObject(Vector2 position, Vector2 size) {
 		physObj = new PhysXObject();
 		physObj.setPosition(position);
-		this.size = size;
+		this.imageSize = size;
 	}
 	
 	public PhysXObject getPhysObj() {
@@ -21,16 +26,20 @@ public class StaticGObject {
 	}
 	
 	public void setSize(Vector2 size) {
-		this.size = size;
+		this.imageSize = size;
 	}
 	
 	public void setup() {
 		this.objects = new GOval[physObj.getColliders().length];
 		for(int i =0; i<this.objects.length; ++i) {
 			Vector2 center = physObj.getColliders()[i].getCenter().add(physObj.getPosition());
-			float size = physObj.getColliders()[i].getRadius();
-			this.objects[i] = new GOval(center.getX() - size, center.getY() - size, size * 2, size * 2);
+			float halfsize = physObj.getColliders()[i].getRadius();
+			this.objects[i] = new GOval(center.getX() - halfsize, center.getY() - halfsize, halfsize * 2, halfsize * 2);
 		}
+	}
+	
+	public void setLocationRespectSize(int coll, Vector2 pos) {
+		this.objects[coll].setLocation(pos.getX() - (imageSize.getX() / 2), pos.getY() - (imageSize.getY() / 2));
 	}
 	
 	public GOval[] getObjects() {
