25b17f7f68916c465e8b0b89f6309262aa821eff
LouisIV <Louislombardoiv@gmail.com>
2017-11-11 18:05:56 -0800

    For drawing static rects

---
+++ src/StaticRect.java
@@ -0,0 +1,26 @@
+import acm.graphics.*;
+public class StaticRect {
+	private Vector2 size;
+	private PhysXObject physObj;
+	private GRect rect;
+	
+	public StaticRect(Vector2 position, Vector2 size) {
+		physObj = new PhysXObject();
+//		physObj.setPosition(new Vector2(position.getX() - size.getX() / 2, position.getY() - size.getY() / 2));
+		physObj.setPosition(position);
+		this.size = size;
+		rect = new GRect(this.size.getX(), this.size.getY());
+	}
+	
+	public PhysXObject getPhysObj() {
+		return this.physObj;
+	}
+	
+	public void setSize(Vector2 size) {
+		this.size = size;
+	}
+	
+	public GRect getRect() {
+		return this.rect;
+	}
+}
