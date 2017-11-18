cdd9c4c898655f2036187b27154539a1f1e9a6e1
liwen <w_li6@u.pacific.edu>
2017-11-18 00:36:51 -0800

    very buggy enemy movement.

--- src/EnemyShip.java
+++ src/EnemyShip.java
@@ -3,16 +3,16 @@ import java.awt.event.ActionListener;
 
 public class EnemyShip extends Ship implements ActionListener{
 	protected static EnemyType type;
+	private static Vector2 target;
 	
 	public EnemyShip(PhysXObject physObj, int current_health, ShipStats stats) {
 		super(physObj, current_health, stats, "PlayerShip-Small.png");
-		
+		target = new Vector2(0,0);
 		// TODO Auto-generated constructor stub
 	}
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// Detect target
-		Vector2 target = new Vector2(0, 0);
 		//BulletManager.shoot(1, 15, BulletType.ENEMY_BULLET, 4, new PhysXObject(), target);
 		
 	}
@@ -23,15 +23,30 @@ public class EnemyShip extends Ship implements ActionListener{
 	public void AIUpdate(Vector2 playerPos) {
 		float MovetoX = playerPos.getX();
 		float MovetoY = playerPos.getY();
-		//dont pass player object, just pass the angle and stuffs of the player.
-		//float differentX = MovetoX;
+		System.out.println("MovetoX: "+ MovetoX+" MovetoY: "+MovetoY);
+		float thisX = this.getPhysObj().getPosition().getX();
+		float thisY = this.getPhysObj().getPosition().getY();
+		float differentX = thisX - MovetoX;
+		float differentY = thisY - MovetoY;
+		System.out.println("DifferentX: "+ differentX+" DifferentY: "+differentY);
+		float angle = (float)Math.atan2(differentX,differentY);
+		System.out.println("Angle: "+angle);
+//		thisX+= this.getStats().getSpeed()*Math.cos(angle);
+//		thisY+= this.getStats().getSpeed()*Math.sin(angle);
+		thisX-= Math.cos(angle);
+		thisY-= Math.sin(angle);
+		this.getPhysObj().setPosition(new Vector2(thisX,thisY));
 	}
 
 	@Override
 	public void Move() {
 		if(getCurrentHealth()>0) {
 			//toward to player
+			AIUpdate(target);
 		}//avoid asteroid method?
 	}
 	
+	public Vector2 getTarget() {
+		return this.target;
+	}
 }
