72dcc7e7be184219e2fa6fee09b9797596f3e645
Anthony Tran <a_tran38@u.pacific.edu>
2017-11-22 20:42:41 -0800

    Added a flashing click to continue

--- src/StoryPane.java
+++ src/StoryPane.java
@@ -16,14 +16,26 @@ public class StoryPane extends GraphicsPane implements ActionListener {
 	private GImage story;
 	private GRect blackBG;
 	private int switchStory;
+	private int colorRed;
+	private int colorBlue;
+	private int colorGreen;
+	private int i;
+	private int j;
+	private int k;
 	
 	public StoryPane(MainApplication app) {
 		program = app;
 		switchStory = 0;
+		colorRed = 0;
+		colorBlue = 0;
+		colorGreen = 0;
+		i = 1;
+		j = 1;
+		k = 1;
 		
 		clickToContinue = new GLabel("CLICK TO CONTINUE...", 0, 0);
 		clickToContinue.setFont(font);
-		clickToContinue.setColor(Color.white);
+		clickToContinue.setColor(new Color(colorRed, colorBlue, colorGreen));
 		clickToContinue.setLocation(5, MainApplication.WINDOW_HEIGHT - 10);
 		
 		story = new GImage("Story_1.png", 0, 0);
@@ -31,13 +43,13 @@ public class StoryPane extends GraphicsPane implements ActionListener {
 		blackBG = new GRect(MainApplication.WINDOW_WIDTH, MainApplication.WINDOW_HEIGHT);
 		blackBG.setFilled(true);
 		blackBG.setColor(Color.black);
-		
-		Timer fadingLabel = new Timer(15, this);
-		fadingLabel.start();
 	}
 	
 	@Override
 	public void showContents() {
+		Timer fadingLabel = new Timer(5, this);
+		fadingLabel.start();
+		
 		switch(switchStory % 3) {
 			case 1:
 				story.setImage("Story_2.png");
@@ -67,5 +79,35 @@ public class StoryPane extends GraphicsPane implements ActionListener {
 	@Override
 	public void mousePressed(MouseEvent e) {
 		program.switchToGame();
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		clickToContinue.setColor(new Color(colorRed, colorGreen, colorBlue));
+        
+        if(colorRed % 256 == 255) {
+            i = -1;
+        }
+        else if(colorRed % 256 == 0) {
+            i = 1;
+        }
+        
+        if(colorGreen % 256 == 255) {
+            j = -1;
+        }
+        else if(colorGreen % 256 == 0) {
+            j = 1;
+        }
+        
+        if(colorBlue % 256 == 255) {
+            k = -1;
+        }
+        else if(colorBlue % 256 == 0) {
+            k = 1;
+        }
+        
+        colorRed += i;
+        colorGreen += j;
+        colorBlue += k;
 	}
 }
