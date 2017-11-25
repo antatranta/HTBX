03444c565875c05d527d38549dc372cb8d403d28
LouisIV <Louislombardoiv@gmail.com>
2017-11-25 10:25:39 -0800

    Lasers

--- src/LaserLine.java
+++ src/LaserLine.java
@@ -25,8 +25,29 @@ public class LaserLine {
 	    return (int)(distance / segmentHeight);
 	}
 	
-	private void createBeam() {
-		
+	private void createBeam(Vector2 origin, Vector2 endPoint) {
+		int numberOfSegments = calculateNumberOfRequiredSegments(origin, endPoint);
+		Vector2 currentPosition = new Vector2(origin);
+		Vector2 movementVector = calcutlateTrajectory(origin, endPoint);
+		for(int i =0; i < numberOfSegments; ++i) {
+			
+//			PhysXObject physObj, String sprite, CollisionData data
+			CircleCollider newCollider = new CircleCollider((float)segmentHeight);
+			PhysXObject newPhysObj = new PhysXObject(new QuadrantID(), newCollider);
+			newPhysObj.setPosition(currentPosition);
+			
+			LaserSegment newSegment = new LaserSegment(newPhysObj, "Laser segment.png", new CollisionData());
+			segments.add(newSegment);
+			
+			currentPosition = currentPosition.add(movementVector.mult(new Vector2((float)segmentHeight, (float)segmentHeight)));
+//			segments.add(e);
+//			Vector2 movement = new Vector2(physObj.getPosition().getX() + getBulletDX(), physObj.getPosition().getY() + getBulletDY());
+//			this.physObj.setPosition(movement);
+		}
+	}
+	
+	private Vector2 calcutlateTrajectory(Vector2 origin, Vector2 endPoint) {
+		return origin.normalize(endPoint);
 	}
 	
 	private void updatePositions() {
