a65ac0404bb885c249c89d56fa08252abed3545f
Anthony Tran <a_tran38@u.pacific.edu>
2017-11-27 20:48:02 -0800

    Scores is stored locally and scorespane is reading scores and sorting.

--- src/ScoresPane.java
+++ src/ScoresPane.java
@@ -1,16 +1,61 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.awt.Color;
 import java.awt.event.MouseEvent;
 
 import acm.graphics.GLabel;
 import acm.graphics.GObject;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+
 public class ScoresPane extends GraphicsPane {
 	private MainApplication program;
+	private ArrayList<String> scores;
+	private ArrayList<GLabel> displayScores;
 	private GLabel back;
 	private GObject obj;
 	
 	public ScoresPane(MainApplication app) {
 		program = app;
+		
+		scores = new ArrayList<String>();
+		String fileName = "Scores.txt";
+		String line = null;
+		
+		try {
+			FileReader fileReader = new FileReader(fileName);
+			
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			
+			while((line = bufferedReader.readLine()) != null) {
+				scores.add(line);
+			}
+			
+			bufferedReader.close();
+		}
+		catch(FileNotFoundException ex) {
+			System.out.println("Unable to open file \"" + fileName + "\"");
+		}
+		catch(IOException ex) {
+			ex.printStackTrace();
+		}
+		
+		Collections.sort(scores, Collections.reverseOrder());
+		displayScores = new ArrayList<GLabel>();
+		
+		int j = 0;
+		for(int i = 0; i < 5; i++) {
+			GLabel temp = new GLabel(scores.get(i));
+			temp.setFont(font);
+			temp.setColor(Color.black);
+			temp.setLocation(CENTER_WIDTH - (temp.getWidth() / 2), CENTER_HEIGHT - (temp.getHeight() / 2) + j);
+			displayScores.add(temp);
+			j += 25;
+		}
+		
 		
 		back = new GLabel("BACK");
 		back.setFont(font);
@@ -22,6 +67,9 @@ public class ScoresPane extends GraphicsPane {
 	public void showContents() {
 		program.add(whiteBG());
 		program.add(title());
+		for(GLabel scoresToDisplay:displayScores) {
+			program.add(scoresToDisplay);
+		}
 		program.add(back);
 		program.add(selection());
 		selection.setLocation(back.getX() - 25, back.getY());
@@ -31,6 +79,9 @@ public class ScoresPane extends GraphicsPane {
 	public void hideContents() {
 		program.remove(whiteBG());
 		program.remove(title());
+		for(GLabel scoresToDisplay:displayScores) {
+			program.remove(scoresToDisplay);
+		}
 		program.remove(back);
 		program.remove(selection());
 	}
